4fced6cb4b2bb736220996f15b7377764b327ade
Mischa Koischwitz <mischa.koischwitz@akquinet.de>
2025-05-27 19:47:06 +0200

    fix diagonal dashes

--- script.js
+++ script.js
@@ -228,12 +228,12 @@ function attemptDash() {
     let dashAngle = 0; // Default to right if no movement keys are pressed
 
     if (keys['w'] || keys['ArrowUp']) {
-      if (keys['a'] || keys['ArrowLeft']) dashAngle = Math.PI * 1.75; // Up-Left
-      else if (keys['d'] || keys['ArrowRight']) dashAngle = Math.PI * 0.25; // Up-Right
+      if (keys['a'] || keys['ArrowLeft']) dashAngle = Math.PI * 1.25; // Up-Left
+      else if (keys['d'] || keys['ArrowRight']) dashAngle = Math.PI * 1.75; // Up-Right
       else dashAngle = Math.PI * 1.5; // Up
     } else if (keys['s'] || keys['ArrowDown']) {
-      if (keys['a'] || keys['ArrowLeft']) dashAngle = Math.PI * 1.25; // Down-Left
-      else if (keys['d'] || keys['ArrowRight']) dashAngle = Math.PI * 0.75; // Down-Right
+      if (keys['a'] || keys['ArrowLeft']) dashAngle = Math.PI * 0.75; // Down-Left
+      else if (keys['d'] || keys['ArrowRight']) dashAngle = Math.PI * 0.25; // Down-Right
       else dashAngle = Math.PI * 0.5; // Down
     } else if (keys['a'] || keys['ArrowLeft']) {
       dashAngle = Math.PI; // Left
